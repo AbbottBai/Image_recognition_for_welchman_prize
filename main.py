5d3d37b2e113ea2741414ec74c994095035421f7
AbbottBai <abbottrun08@hotmail.com>
2025-05-13 09:44:18 +0100

    Finished first edition of relu hidden layers. Neurons need to be changed, one weight and bias per pixel for all neurons instead of one weight and one bias for each neuron. Also there are too many layers causing gradient explosion from dying neurons.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import cv2
 import numpy as np
 import os
+import random
 
-number_of_photos = 10
-number_of_people = 4
+number_of_photos = 1000
+number_of_people = 3
 
 def take_photos():
+    print("Commencing photo capturing")
     for a in range(number_of_people):
         inp = input("Please hit enter once a new person is in front of the camera")
         output_dir = str(a)
@@ -14,7 +16,6 @@ def take_photos():
         # Open the webcam (0 = default camera)
         cap = cv2.VideoCapture(0)
 
-        # Take 5000 photos
         for i in range(number_of_photos):
             ret, frame = cap.read()  # Capture a frame
             if ret:
@@ -34,6 +35,7 @@ def take_photos():
 
         cap.release()
         cv2.destroyAllWindows()
+    print("Photo capturing complete")
 
 
 # This is also used in normal distribution!
@@ -62,133 +64,116 @@ def standardization(img):
     return std_img
 
 
-def five_by_five(input_matrix):
-    size = input_matrix.shape
-    if size[0] != size[1]:
-        print("Matrix is not a square, 5x5 function cannot be executed")
-
-    else:
-        num_squares = size[0] // 5
-        output_matrix = [[[] for _ in range(num_squares)] for _ in range(num_squares)]
-        for i in range (num_squares): #Vertical
-            for j in range(num_squares): #Horizontal
-                for k in range(5): #Individual vertical levels
-                    for l in range(5): #Individual values
-                        output_matrix[i][j].append(input_matrix[i * 5 + k][j * 5 + l])
-        output_matrix = np.array(output_matrix)
-        return output_matrix
-
-
-# Pass in all images instead of just one image at a time
-# Pass in the whole picture, not just a 5x5 area
-# Self.x and self.y will be 3D numpy arrays. They will represent all images.
-# W will be a 2D array, so will B. They will be local to the image.
-# REMEMBER, this is ONE LAYER FOR ALL PHOTOS
-class logistic_regression():
-    def __init__(self, x):
-        self.x = x # 4D array, 0 = image of person, 1 = number of 5x5 rows, 2 = number of 5x5 columns, 3 = number of pixels in 5x5 (25)
-        self.x_shape = x.shape
-        self.w = np.ones((self.x_shape[1], self.x_shape[2])) # 2D array, One W for each (5x5) area of an image
-        self.b = np.ones((self.x_shape[1], self.x_shape[2]))
-        self.sigmoid = np.zeros((self.x_shape[1], self.x_shape[2])) # For one whole image
-        self.all_sigmoid = np.zeros((self.x_shape[0], self.x_shape[1], self.x_shape[2])) # 3D array containing sigmoid of all areas of all images
-        self.cost = np.ones((self.x_shape[1], self.x_shape[2]))
-        self.alpha = 0.0001
+class relu():
+    def __init__(self, x, num_neurons, alpha):
+        self.x = x # 0 = person, 1 = image of person, 2 = number of rows, 3 = number of columns
+        self.num_neurons = num_neurons
+        self.sqrt_neurons = int(np.sqrt(self.num_neurons))
+        self.w = [[random.uniform(-0.4, 0.4) for _ in range(self.sqrt_neurons)] for _ in range(self.sqrt_neurons)]
+        # W is set to random value to prevent symmetry between all neurons.
+        # B is set to 0 as W already prevents symmetry
+        self.b = [[0 for _ in range(self.sqrt_neurons)] for _ in range(self.sqrt_neurons)]
+        self.output = [[[[0 for _ in range(self.sqrt_neurons)] for _ in range(self.sqrt_neurons)] for _ in range(self.x.shape[1])] for _ in range(self.x.shape[0])]
+        self.alpha = alpha
 
     def forward_prop(self):
-        # Calculate the total sigmoid of one image, and then concatenate sigmoid of all of the images at the end.
-        for i in range (self.x_shape[0]): # Loop through all images
-            for j in range(self.x_shape[1]): # For all row boxes of one image
-                for k in range(self.x_shape[2]): # For all column boxes of one image
-                    self.sigmoid[j][k] = 1 / (1 + np.e ** (-(self.w[j][k] * sum(self.x[i][j][k]) + self.b[j][k])))
+        for i in range(self.x.shape[0]):
+            for j in range(self.x.shape[1]):
+                total_x = np.sum(self.x[i][j])
 
-            self.all_sigmoid[i] = self.sigmoid
+                for k in range(self.sqrt_neurons):
+                    for l in range(self.sqrt_neurons):
+                        func = self.w[k][l] * total_x + self.b[k][l]
+                        if func > 0:
+                            self.output[i][j][k][l] = func
 
-        return self.all_sigmoid
+        output = np.array(self.output)
+        return output
 
+    def back_prop(self, prev_d): # Implement matmul? Image by image
+        sum_weights = np.sum(self.w)
+        current_d = [[0 for _ in range(self.x.shape[1])] for _ in range(self.x.shape[0])] # 2D array, one per image
+        for a in range(self.x.shape[0]):
+            for b in range(self.x.shape[1]):
+                total_x = np.sum(self.x[a][b]) # Sum of pixels in one image
 
-    def back_prop(self, prev_d):
-        # Prev_d is a 1D array for all areas of the image
-        self.forward_prop()
-        current_d = [[[0 for _ in range(self.x_shape[2] * 5)] for _ in range(self.x_shape[1] * 5)] for _ in range(self.x_shape[0])]
-        for i in range(self.x_shape[0]): # For all images
-            for j in range(self.x_shape[1]): # For each 5x5 row of an image
-                for k in range(self.x_shape[2]): # For each 5x5 column of an image
-                    total_x = 0
-                    for l in range(self.x_shape[3]): # Find the average value that the current w changes the final result.
-                        total_x += self.x[i][j][k][l]
+                for i in range(len(self.w[0])):
+                    for j in range(len(self.w[1])):
+                        old_w = self.w[i][j]
+                        self.w[i][j] -= self.alpha * prev_d[a][b] * old_w * (total_x / (self.x.shape[2] * self.x.shape[3])) # Update w and b for all images at once to prevent w and b overfitting and changing drastically to one pixel
+                        self.b[i][j] -= self.alpha * prev_d[a][b] * old_w
 
-                    self.w[j][k] -= self.alpha * prev_d[i][j][k] * self.sigmoid[i][j][k] * (1 - self.sigmoid[i][j][k]) * (total_x/self.x_shape[3])
-                    self.b[j][k] -= self.alpha * prev_d[i][j][k] * self.sigmoid[i][j][k] * (1 - self.sigmoid[i][j][k])
-                    # Iteration to create new 5x5 areas with repeating current_d
-                    # Current_d will be a 3D matrix, unique for each image
-                    for y in range(5):
-                        for z in range(5):
-                            current_d[i][5*j+y][5*k+z] = ([prev_d[j][k] * self.sigmoid[i][j][k] * (1 - self.sigmoid[i][j][k])])
-                            # There is a photo in your icloud if you forgot how this works
-        current_d = np.array(current_d)
+                current_d[a][b] = prev_d[a][b] * sum_weights
+
         return current_d
 
-
 class softmax():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y # Integer value for index of each face/a face, starting from ZERO!!!
-        self.x_shape = x.shape
-        self.w = np.ones((self.x_shape[1], self.x_shape[2]))
-        self.b = np.ones((self.x_shape[1], self.x_shape[2]))
-        self.softmax = np.zeros((self.x_shape[1], self.x_shape[2]))  # For one whole image
-        self.all_softmax = [] # 3D array containing softmax of all areas of all images
-        self.alpha = 0.0001
+    def __init__(self, x, num_neurons, alpha):
+        self.x = x # 0 = person, 1 = image of person, 2 = number of rows, 3 = number of columns
+        self.num_neurons = num_neurons
+        self.w = [random.uniform(-0.4, 0.4) for _ in range(self.num_neurons)]
+        self.b = [0 for _ in range(self.num_neurons)]
+        self.linear_func = [[[0 for _ in range(self.num_neurons)] for _ in range(self.x.shape[1])] for _ in range(self.x.shape[0])]
+        self.output = [[[0 for _ in range(num_neurons)] for _ in range(self.x.shape[1])] for _ in range(self.x.shape[0])]
+        self.y = int() # Start index from 0
+        self.alpha = alpha
 
     def forward_prop(self):
-        func = np.zeros((self.x_shape[0], self.x_shape[1], self.x_shape[2])) # 3D array containing all areas of all images
-        for i in range (self.x_shape[0]): # Loop through all images
-            func_sum = 0
-            for k in range(self.x_shape[1]):  # For sum of functions of all rows
-                for a in range(self.x_shape[2]): # For sum of functions of all columns
-                    func_sum += np.e ** (self.w[k][a] * sum(self.x[i][k][a]) + self.b[k][a]) # Function for one area
-            for j in range(self.x_shape[1]): # For each row
-                for b in range(self.x_shape[2]): # For each column
-                    func[i][j][b] = self.w[j][b] * sum(self.x[i][j][b]) + self.b[j][b] # Function for one area
-                    self.softmax[j][b] = (np.e ** func[i][j][b]) / func_sum
+        for i in range(self.x.shape[0]):
+            for j in range(self.x.shape[1]):
+                total_x = 0.0
+                total_x2 = 0.0
+                std_x = 0.0
+                for a in range(self.x.shape[2]):
+                    for b in range(self.x.shape[3]):
+                        total_x += self.x[i][j][a][b]
+                        total_x2 += self.x[i][j][a][b] ** 2
+                # Normal standardization is implimented so the exponent doesn't get too large and uncomputable
+                mean_x = total_x / (self.x.shape[0] * self.x.shape[1] * self.x.shape[2] * self.x.shape[3])
+                std_dev =  np.sqrt((total_x2/(self.x.shape[2] * self.x.shape[3])) - mean_x ** 2)
+                for c in range(self.x.shape[2]):
+                    for d in range(self.x.shape[3]):
+                        std_x += (self.x[i][j][c][d] - mean_x) / std_dev
+                for k in range(self.num_neurons):
+                    self.linear_func[i][j][k] = self.w[k] * std_x + self.b[k]
 
-            self.all_softmax.append(self.softmax)
+        for f in range(self.x.shape[0]):
+            for g in range(self.x.shape[1]):
+                denominator = 0.0
+                for z in range(self.num_neurons):
+                    denominator += np.exp(self.linear_func[f][g][z])
+                for h in range(self.num_neurons):
+                    self.output[f][g][h] = np.exp(self.linear_func[f][g][h]) / denominator
 
-        self.all_softmax = np.array(self.all_softmax)
-        return self.all_softmax
+    def cost(self): # Image by image
+        cost = [[0 for _ in range(self.x.shape[1])] for _ in range(self.x.shape[0])] # 2D array, one per image
+        for i in range(self.x.shape[0]):
+            self.y = i
+            for j in range(self.x.shape[1]):
+                cost[i][j] = -(np.log(self.output[i][j][self.y]))
 
-    def cost(self):
-        loss = 0
-        for i in range(self.x_shape[0]):
-            loss += -np.log(self.all_softmax[self.y])
+        total_cost = np.sum(cost) / (self.x.shape[1] * self.x.shape[0])
 
-        cost = loss / self.x_shape[0]
-        return cost
+        return cost, total_cost
 
-    def gradient_descent(self):
-        for i in range(self.x_shape[0]):
-            # Line below is to make the gradient of the loss of the actual value negative whilst keeping others positive
-            # So it reduces the weight for wrong predictions but increase the weight for right predictions later on...
-            self.all_softmax[i][self.y] -= 1
+    def gradient_descent(self): # Image by image
+        for i in range(self.x.shape[0]):
+            self.y = i
+            for j in range(self.x.shape[1]):
+                self.output[i][j][self.y] -= 1
+                # Line above is to make the gradient of the loss of the actual value negative whilst keeping the others positive
+                # So it reduces the weight for wrong predictions but increases the weight for right predictions later on...
+                total_x = sum(self.x[i][j])
 
-            for j in range(self.x_shape[1] * self.x_shape[2]):
-                total_x = 0
-                for k in range(self.x_shape[2]):
-                    total_x += self.x[i][j][k]
-
-                actual_probability = 0
-                if self.y == j:
-                    actual_probability = 1
-                self.w[j] -= self.alpha * (self.all_softmax[i][j] - actual_probability) * (total_x / self.x_shape[2])
-                self.b[j] -= self.alpha * (self.all_softmax[i][j] - actual_probability)
+                for m in range(self.num_neurons):
+                    self.w[m] -= self.alpha * self.output[i][j][m] * (total_x / (self.x.shape[2] * self.x.shape[3]))
+                    self.b[m] -= self.alpha * self.output[i][j][m]
 
 
 def train():
-    #take_photos()
-    print("Photo taking complete, commencing photo processing")
-    all_photos = [] # This will be a 5D array, with all photos taken from different people
-    # 0 = person, 1 = image of person, 2 = number of 5x5 rows, 3 = number of 5x5 columns, 4 = number of pixels in 5x5 (25)
+    print("commencing photo processing")
+    all_photos = [] # This will be a 4D array, with all photos taken from different people
+    # 0 = person, 1 = image of person, 2 = number of rows, 3 = number of columns
     for i in range(number_of_people):
         all_photos.append([])
         for j in range(number_of_photos):
@@ -196,7 +181,6 @@ def train():
                 image_path = os.path.join(str(i), f"image{j}.png")
                 img = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
                 img = standardization(img)
-                img = five_by_five(img)
                 all_photos[i].append(img)
             except Exception as e:
                 print(f"Error processing image {j} from subdirectory {i}: {e}")
@@ -208,37 +192,82 @@ def train():
 
     run = True
     total_iteration = 0
-    prev_cost = 999
+    total_cost = 100
     while run:
         total_iteration += 1
-        for i in range(number_of_people):
-            # print(all_photos.shape), in case you need a reminder for what the all_image array looks like
+        if total_cost <= 20:
+            alpha = 0.0001
+        else:
+            alpha = 0.001
+        hidden1 = relu(all_photos, 4096, alpha)
+        a1 = hidden1.forward_prop()
 
-            input_layer = logistic_regression(all_photos[i])
-            a1 = input_layer.forward_prop()
-            a1 = five_by_five(a1)
+        hidden2 = relu(a1, 2025, alpha)
+        a2 = hidden2.forward_prop()
 
-            hidden1 = logistic_regression(a1)
-            a2 = hidden1.forward_prop()
-            a2 = five_by_five(a2)
+        hidden3 = relu(a2, 1024, alpha)
+        a3 = hidden3.forward_prop()
 
-            hidden2 = logistic_regression(a2)
-            a3 = hidden2.forward_prop()
-            a3 = five_by_five(a3)
+        hidden4 = relu(a3, 529, alpha)
+        a4 = hidden4.forward_prop()
 
-            output = softmax(a3, i)
-            cost = output.cost()
-            print(f"Iteration: {total_iteration}, cost: {cost}, person: {i}")
+        hidden5 = relu(a4, 256, alpha)
+        a5 = hidden5.forward_prop()
 
-            output.gradient_descent()
-            current_d = hidden2.back_prop(cost)
-            current_d = hidden1.back_prop(current_d)
-            current_d = input_layer.back_prop(current_d)
+        hidden6 = relu(a5, 121, alpha)
+        a6 = hidden6.forward_prop()
 
-            if prev_cost - cost <= 0.01:
-                print("Training complete")
-                run = False
+        output = softmax(a6, number_of_people, alpha)
+        output.forward_prop()
+        cost, total_cost = output.cost()
+        print(f"Iteration: {total_iteration}, cost: {total_cost}")
+
+        output.gradient_descent()
+        current_d = hidden6.back_prop(cost)
+        print("output layer backprop complete")
+        current_d = hidden5.back_prop(current_d)
+        print("hidden 5 backprop complete")
+        current_d = hidden4.back_prop(current_d)
+        print("hidden 4 backprop complete")
+        current_d = hidden3.back_prop(current_d)
+        print("hidden 3 backprop complete")
+        current_d = hidden2.back_prop(current_d)
+        print("hidden 2 backprop complete")
+        current_d = hidden1.back_prop(current_d)
+        print("hidden 1 backprop complete")
+        if total_cost <= 0.001:
+            run = False
+
+
+def predict():
+    run = True
+    counter = 0
+    while run:
+        inp = input("press t to take a photo, or q to quit").lower()
+        if inp[0] == "q":
+            run = False
+        else:
+            cap = cv2.VideoCapture(0)
+            ret, frame = cap.read()
+            if ret:
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                resized_gray = cv2.resize(gray, (250, 250))
+                resized_gray = resized_gray.astype(np.float32)
+                # Casts the original image into float, so that overflow does not occur due to negative and decimal calculations
+                # being carried out on integers.
+
+                filename = os.path.join(os.path.dirname(__file__), f"{counter}.png")
+                cv2.imwrite(filename, resized_gray)  # Save the frame to file
+                print(f"Saved {filename}")
+                cv2.imshow('Captured Image', frame)  # Optional: show the photo
+
+                image = f"{counter}.png"
+
             else:
-                prev_cost = cost
+                print("Failed to capture image")
 
+        counter += 1
+
+
+#take_photos()
 train()
